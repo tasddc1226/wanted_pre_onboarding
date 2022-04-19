895b6bba728455731a6cdad18eafa01d438acee8
yang_swim <tasddc@naver.com>
2022-04-20 00:39:16 +0900

    :recycle: ViewSet 적용 CRUD 리펙터링

--- practice/crowd/views.py
+++ practice/crowd/views.py
@@ -1,53 +1,7 @@
-from django.shortcuts import get_object_or_404, redirect, render
-from rest_framework.response import Response
-from rest_framework.views import APIView
-from rest_framework import status
-from django.http import Http404
+from .models import Product
 from .serializers import ProductSerializer
+from rest_framework import viewsets
 
-from .models import *
-from .form import *
-
-class ProductListAPI(APIView):
-    # 모든 상품의 목록 가져오기
-    def get(self, request):
-        products = Product.objects.all()
-        serializer = ProductSerializer(products, many=True)
-        return Response(serializer.data)
-    
-    # 새로운 상품 등록하기
-    def post(self, request):
-        serializer = ProductSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-class ProductDetail(APIView):
-    # Product 객체 가져오기
-    def get_object(self, pk):
-        try:
-            return Product.objects.get(pk=pk)
-        except Product.DoesNotExist:
-            raise Http404
-    
-    # 한 상품의 detail 확인
-    def get(self, request, pk, format=None):
-        product = self.get_object(pk)
-        serializer = ProductSerializer(product)
-        return Response(serializer.data)
-
-    # 상품 정보 수정하기
-    def put(self, request, pk, format=None):
-        product = self.get_object(pk)
-        serializer = ProductSerializer(product, data=request.data)
-        if (serializer.is_valid()):
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
-    # 상품 삭제하기
-    def delete(self, request, pk, format=None):
-        product = self.get_object(pk)
-        product.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+class ProductViewSet(viewsets.ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
