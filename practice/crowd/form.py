df6e31328817416cc63fc9686026e16ebd1c5801
yang_swim <tasddc@naver.com>
2022-04-20 00:38:11 +0900

    :fire: form.py  삭제

--- practice/crowd/form.py
+++
@@ -1,8 +0,0 @@
-
-from django import forms
-from .models import Product
-
-class ProductForm(forms.ModelForm):
-    class Meta:
-        model = Product
-        fields = ['title', 'writer', 'description', 'target_price', 'expiry_date', 'funding_cost']
