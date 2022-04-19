d72c6beada5a1bbb77ff3dc5c864dc56b4462584
yang_swim <tasddc@naver.com>
2022-04-19 22:36:58 +0900

    :label: 펀딩 상품 모델 추가

--- practice/crowd/models.py
+++ practice/crowd/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    title = models.CharField(max_length=200)
+    writer = models.CharField(max_length=100)
+    description = models.TextField()
+    target_price = models.IntegerField()
+    expiry_date = models.DateTimeField()
+    funding_cost = models.IntegerField()
+
+    def __str__(self):
+        return self.title
+
+    def summary(self):
+        return self.description[:10]
