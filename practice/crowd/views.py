e31a7fb33381917503f80950cb3ef16a16bd4ec2
yang_swim <tasddc@naver.com>
2022-04-19 22:38:29 +0900

    :label: 라우팅 경로 설정 및 views.py 파일 update

--- practice/crowd/views.py
+++ practice/crowd/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, redirect, render
+
+from .models import *
+from .form import *
 
 # Create your views here.
+def home(request):
+    products = Product.objects.all()
+    return render(request, 'home.html', {'products': products})
+
+def new(request):
+    form = ProductForm()
+    return render(request, 'new.html', {'form' : form})
+
+def create(request):
+    form = ProductForm(request.POST, request.FILES)
+    if form.is_valid():
+        new_product = form.save(commit=False)
+        new_product.save()
+        return redirect('detail', new_product.id)
+    return redirect('home')
+
+def detail(request, id):
+    # details = Product.objects.get(pk=id)
+    product = get_object_or_404(Product, pk=id)
+    return render(request, 'detail.html', {'product': product})
+
+    
--- practice/practice/urls.py
+++ practice/practice/urls.py
@@ -16,6 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from crowd.views import *
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home, name='home'),
+    path('<str:id>', detail, name='detail'),
+    path('new/', new, name='new'),
+    path('create/', create, name='create'),
 ]
