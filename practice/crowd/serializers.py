2aa1aa9196ca077c0bc1c5284f8c44ec92807bda
yang_swim <tasddc@naver.com>
2022-04-20 13:04:38 +0900

    :sparkles: 상품 상세 목록, 수정 가능 feild 구분

--- practice/crowd/serializers.py
+++ practice/crowd/serializers.py
@@ -4,4 +4,38 @@ from .models import Product
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
-        fields = '__all__'
+        fields = [
+            'id',
+            'title',
+            'writer',
+            'description',
+            'target_price',
+            'expiry_date',
+            'funding_cost',
+            'total_cost',
+        ]
+        read_only_fields = ('participants', 'total_cost')
+
+class ProductDetailSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Product
+        fields = [
+            'title',
+            'writer',
+            'total_cost',
+            'getAchievement_rate',
+            'getD_day',
+            'description',
+            'target_price',
+            'getParticipants',
+            'expiry_date',
+            'funding_cost',
+        ]
+
+        read_only_fields = ('target_price', 'total_cost')
+
+        extra_kwargs = {
+            'expiry_date' : {'write_only': True},
+            'funding_cost': {'write_only': True}
+        }
--- practice/crowd/views.py
+++ practice/crowd/views.py
@@ -1,7 +1,15 @@
 from .models import Product
-from .serializers import ProductSerializer
-from rest_framework import viewsets
+from .serializers import ProductSerializer, ProductDetailSerializer
+from rest_framework import viewsets, filters, generics, status
 
-class ProductViewSet(viewsets.ModelViewSet):
+class ProductViewSet(generics.ListCreateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+
+    order_fields = ['created_at', 'total_cost']
+    search_fields = ['title']
+    filter_func = (filters.SearchFilter, filters.OrderingFilter)
+
+class ProductDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductDetailSerializer
