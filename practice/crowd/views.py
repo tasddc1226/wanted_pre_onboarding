b9d1f6ae17842ecda15e934b5b7f2571598368b8
yang_swim <tasddc@naver.com>
2022-04-20 13:19:08 +0900

    :sparkles: 상품 검색, 만료 기간 순 정렬 기능 수정

--- practice/crowd/views.py
+++ practice/crowd/views.py
@@ -1,14 +1,15 @@
 from .models import Product
 from .serializers import ProductSerializer, ProductDetailSerializer
-from rest_framework import viewsets, filters, generics, status
+from rest_framework import viewsets, generics, status
+from rest_framework.filters import SearchFilter, OrderingFilter
 
 class ProductViewSet(generics.ListCreateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
 
-    order_fields = ['created_at', 'total_cost']
+    ordering_fields = ['created_at', 'total_cost']
     search_fields = ['title']
-    filter_func = (filters.SearchFilter, filters.OrderingFilter)
+    filter_backends = (SearchFilter, OrderingFilter)
 
 class ProductDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Product.objects.all()
