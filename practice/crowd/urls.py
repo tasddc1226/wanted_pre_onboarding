98fe712ef3352413f126719a6ba25a2b2f36ae54
yang_swim <tasddc@naver.com>
2022-04-20 14:12:35 +0900

    :pencil2: 펀딩 Url 등록

--- practice/crowd/urls.py
+++ practice/crowd/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path, include
-from .views import ProductViewSet, ProductDetailView
+from .views import ProductViewSet, ProductDetailView, ProductFundingView
 
 urlpatterns = [
     path('product/', ProductViewSet.as_view()),
-    path('product/<int:pk>', ProductDetailView.as_view())
+    path('product/<int:pk>', ProductDetailView.as_view()),
+    path('product/<int:pk>/funding', ProductFundingView.as_view())
 ]
 
