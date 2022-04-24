47c2651bcad84a47bf06ac3338afd2770d5cd986
yang_swim <tasddc@naver.com>
2022-04-24 14:38:30 +0900

    :sparkles: users app 추가

--- practice/practice/settings.py
+++ practice/practice/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'crowd.apps.CrowdConfig',
+    'crowd',
+    'users',
     'rest_framework'
 ]
 
